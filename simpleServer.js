d06d2475c7ebcd9e1d233c7292169115c85caa14
katcy <42054792+katcy@users.noreply.github.com>
2018-08-03 10:44:25 +0530

    Update simpleServer.js

--- simpleServer.js
+++ simpleServer.js
@@ -2,7 +2,7 @@
 /*
 *
 */
-// var http = require("http");
+var http = require("http");
 // var fs = require("fs");
 // var server = http.createServer(function(req,res){
 //     fs.readFile('index.html',function(err,data){
